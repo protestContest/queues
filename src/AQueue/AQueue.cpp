85a191e43a04479e7f18e3245c6af1f8b17e643b
Zack Michener <zack@zackmichener.com>
2012-09-19 22:49:34 -0400

    created stubbed AQueue

--- src/AQueue/AQueue.cpp
+++ src/AQueue/AQueue.cpp
@@ -1,1 +1,21 @@
+#include "./AQueue.h"
 
+AQueue::AQueue() {
+
+}
+
+AQueue::~AQueue() {
+
+}
+
+void AQueue::enqueue(int i) {
+	i++;
+}
+
+int AQueue::dequeue() {
+	return 0;
+}
+
+int AQueue::size() {
+	return 0;
+}
---
+++ src/AQueue/AQueue.h
@@ -0,0 +1,11 @@
+class AQueue {
+	private:
+	
+	public:
+	AQueue();
+	~AQueue();
+	void enqueue(int i);
+	int dequeue();
+	int size();
+
+};

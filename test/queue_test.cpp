fdc38e1012ec4c0ecb0c5f22fc726c8fb6211440
Zack Michener <zack@zackmichener.com>
2012-09-19 22:32:52 -0400

    wrote tests for AQueue

--- test/queue_test.cpp
+++ test/queue_test.cpp
@@ -1,5 +1,37 @@
 #include "gtest/gtest.h"
+#include "AQueue/AQueue.h"
 
-TEST(QueueTest, Constructor) {
-	EXPECT_EQ(1, 1);
+TEST(AQueueTest, Constructor) {
+	AQueue* q = new AQueue();
+
+	EXPECT_EQ(q->size(), 0);
+
+	delete q;
 }
+
+TEST(AQueueTest, Enqueue) {
+	AQueue* q = new AQueue();
+
+	for (int i = 0; i < 150; ++i) {
+		q->enqueue(i);
+	}
+
+	EXPECT_EQ(q->size(), 150);
+}
+
+TEST(AQueueTest, EnqueueDequeue) {
+	AQueue* q = new AQueue();
+
+	for (int i = 1; i <= 15; ++i) {
+		q->enqueue(i);
+	}
+
+	EXPECT_EQ(q->dequeue(), 15);
+
+	for (int i = 13; i <= 1; ++i) {
+		EXPECT_EQ(i, q->dequeue());
+	}
+
+	delete q;
+}
+

b13780289a4ee996ee2147007739e2be89ecfa2e
Zack Michener <zack@zackmichener.com>
2012-09-21 10:27:04 -0400

    copied tests for LQueue and VQueue

--- test/queue_test.cpp
+++ test/queue_test.cpp
@@ -1,5 +1,7 @@
 #include "gtest/gtest.h"
 #include "AQueue/AQueue.h"
+#include "LQueue/LQueue.h"
+#include "VQueue/VQueue.h"
 
 TEST(AQueueTest, Constructor) {
 	AQueue* q = new AQueue();
@@ -52,3 +54,108 @@ TEST(AQueueTest, IsEmpty) {
 
 	delete q;
 }
+
+TEST(LQueueTest, Constructor) {
+	LQueue* q = new LQueue();
+
+	EXPECT_EQ(q->size(), 0);
+
+	delete q;
+}
+
+TEST(LQueueTest, Enqueue) {
+	LQueue* q = new LQueue();
+
+	for (int i = 0; i < 150; ++i) {
+		q->enqueue(i);
+	}
+
+	EXPECT_EQ(q->size(), 150);
+}
+
+TEST(LQueueTest, Dequeue) {
+	LQueue* q = new LQueue();
+
+	for (int i = 1; i <= 15; ++i) {
+		q->enqueue(i);
+	}
+
+	for (int i = 15; i <= 1; --i) {
+		EXPECT_EQ(i, q->dequeue());
+	}
+
+	delete q;
+}
+
+TEST(LQueueTest, IsEmpty) {
+	LQueue* q = new LQueue();
+
+	EXPECT_EQ(true, q->isEmpty());
+
+	for (int i = 0; i < 50; ++i) {
+		q->enqueue(i);
+	}
+
+	EXPECT_EQ(false, q->isEmpty());
+
+	for (int i = 0; i < 50; ++i) {
+		q->dequeue();
+	}
+
+	EXPECT_EQ(true, q->isEmpty());
+
+	delete q;
+}
+/*
+TEST(VQueueTest, Constructor) {
+	VQueue* q = new VQueue();
+
+	EXPECT_EQ(q->size(), 0);
+
+	delete q;
+}
+
+TEST(VQueueTest, Enqueue) {
+	VQueue* q = new VQueue();
+
+	for (int i = 0; i < 150; ++i) {
+		q->enqueue(i);
+	}
+
+	EXPECT_EQ(q->size(), 150);
+}
+
+TEST(VQueueTest, Dequeue) {
+	VQueue* q = new VQueue();
+
+	for (int i = 1; i <= 15; ++i) {
+		q->enqueue(i);
+	}
+
+	for (int i = 15; i <= 1; --i) {
+		EXPECT_EQ(i, q->dequeue());
+	}
+
+	delete q;
+}
+
+TEST(VQueueTest, IsEmpty) {
+	VQueue* q = new VQueue();
+
+	EXPECT_EQ(true, q->isEmpty());
+
+	for (int i = 0; i < 50; ++i) {
+		q->enqueue(i);
+	}
+
+	EXPECT_EQ(false, q->isEmpty());
+
+	for (int i = 0; i < 50; ++i) {
+		q->dequeue();
+	}
+
+	EXPECT_EQ(true, q->isEmpty());
+
+	delete q;
+}
+*/

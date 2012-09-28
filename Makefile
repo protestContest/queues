8f767ac9656911b9a701dc7dec610055a0e40e9c
Zack Michener <zack@zackmichener.com>
2012-09-28 17:33:44 -0400

    implemented time tests

--- Makefile
+++ Makefile
@@ -7,15 +7,18 @@ GTEST_MAINA = $(GTEST)/make/gtest_main.a
 GTEST_INCLUDES = -I$(GTEST) -I$(GTEST)/include
 
 QTEST = $(TEST)/queue_test.o
-AQUEUE = $(SRC)/AQueue/Queue.o
-LQUEUE = $(SRC)/LQueue/Queue.o
-VQUEUE = $(SRC)/VQueue/Queue.o
+TIME = $(SRC)/time/time.o
 
 CPP = g++
 CPPFLAGS = -Wall -Wextra
 
 
-all: aqueue_test lqueue_test vqueue_test
+all: tests time
+
+
+tests: aqueue_test lqueue_test vqueue_test
+
+time: aqueue_time lqueue_time vqueue_time
 
 aqueue_test: aqueue $(QTEST) $(GTEST_MAINA)
 	$(CPP) $(CPPFLAGS) $(GTEST_MAINA) $(QTEST) $(SRC)/Queue.o \
@@ -30,8 +33,21 @@ lqueue_test: lqueue $(QTEST) $(GTEST_MAINA)
 			-pthread -o $(BUILD)/lqueue_test
 
 
+aqueue_time: aqueue $(TIME)
+	$(CPP) $(CPPFLAGS) $(SRC)/Queue.o $(TIME) -o $(BUILD)/aqueue_time
+
+vqueue_time: vqueue $(TIME)
+	$(CPP) $(CPPFLAGS) $(SRC)/Queue.o $(TIME) -o $(BUILD)/vqueue_time
+
+lqueue_time: lqueue $(TIME)
+	$(CPP) $(CPPFLAGS) $(SRC)/Queue.o $(SRC)/Node.o $(TIME) -o $(BUILD)/lqueue_time
+
+
 $(QTEST): $(TEST)/queue_test.cpp
 	cd $(TEST) && $(MAKE) queue_test
+
+$(TIME): $(SRC)/time/time.cpp
+	cd $(SRC)/time && $(MAKE)
 
 aqueue: $(SRC)/AQueue/AQueue.cpp
 	cd $(SRC)/AQueue && $(MAKE)
@@ -55,6 +71,6 @@ clean:
 	$(MAKE) -C $(SRC)/LQueue clean
 	$(MAKE) -C $(SRC)/VQueue clean
 	$(MAKE) -C $(TEST) clean
-	rm -rf $(BUILD)/*queue_test
+	rm -rf $(BUILD)/*
 	rm -rf $(SRC)/*.o
 	rm -rf $(SRC)/*.h
---
+++ src/time/time.cpp
@@ -0,0 +1,77 @@
+#include "../Queue.h"
+#include <ctime>
+#include <iostream>
+#include <vector>
+using std::cout;
+using std::endl;
+using std::vector;
+
+int main() {
+	Queue* q = new Queue();
+	int start, end, elapsed, iterations;
+	double seconds;
+	iterations = 1000000;
+
+	// Enqueue
+	cout << "Average enqueue time: ";
+	for (int i = 0; i < iterations; ++i) {
+		start = clock();
+		q->enqueue(i);
+		end = clock();
+		elapsed = end - start;
+		seconds += (double) elapsed/CLOCKS_PER_SEC;
+	}
+	seconds /= iterations;
+
+	cout << seconds << endl;
+
+	// Dequeue
+	seconds = 0;
+	cout << "Average dequeue time: ";
+	for (int i = 0; i < iterations; ++i) {
+		start = clock();
+		q->dequeue();
+		end = clock();
+		elapsed = end - start;
+		seconds += (double) elapsed/CLOCKS_PER_SEC;
+	}
+	seconds /= iterations;
+
+	cout << seconds << endl;
+	delete q;
+
+	// Size
+	q = new Queue();
+	seconds = 0;
+	cout << "Average size time: ";
+	for (int i = 0; i < iterations; ++i) {
+		q->enqueue(i);
+		start = clock();
+		q->size();
+		end = clock();
+		elapsed = end - start;
+		seconds += (double) elapsed/CLOCKS_PER_SEC;
+	}
+	seconds /= iterations;
+
+	cout << seconds << endl;
+	delete q;
+
+	// isEmpty
+	q = new Queue();
+	seconds = 0;
+	cout << "Average isEmpty time: ";
+	for (int i = 0; i < iterations; ++i) {
+		q->enqueue(i);
+		start = clock();
+		q->isEmpty();
+		end = clock();
+		elapsed = end - start;
+		seconds += (double) elapsed/CLOCKS_PER_SEC;
+	}
+	seconds /= iterations;
+
+	cout << seconds << endl << endl;
+
+	delete q;
+}
